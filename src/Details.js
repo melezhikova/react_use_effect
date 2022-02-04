530b8acdb9c96491e680ac010a77ffb4a584d241
Liudmila Melezhikova <melezhikova@yandex.ru>
2022-02-04 22:39:25 +0500

    second commit

--- src/Details.js
+++ src/Details.js
@@ -1,21 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Details (props) {
     const { info } = props;
+
     const [id, setId] = useState(null);
     const [data, setData] = useState(null);
+    const [isLoading, setLoading] = useState(false);
+    const [hasError, setError] = useState(null);
+    const timestampRef = useRef();
 
     useEffect(() => {
         setId(prevId => prevId === info.id ? prevId : info.id);
     }, [info]);
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_LIST_URL}${id}.json`)
-        .then(response=>response.json())
-        .then(data => {
-            setData(data)
-            console.log(data);
-        })
+        const fetchData = async() => {
+            const timestamp = Date.now();
+            timestampRef.current = timestamp;
+            setLoading(true);
+            try {
+                const response = await fetch(`${process.env.REACT_APP_LIST_URL}${id}.json`);
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                if (timestampRef.current === timestamp) {
+                    const data = await response.json();
+                    setData(data);
+                }
+                setError(null);
+            } catch (e) {
+                setError(e);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
     }, [id]);
 
     return (
